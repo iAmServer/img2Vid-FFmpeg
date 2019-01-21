c247b17dce1828146b0952270a994fc4f9f4a8b0
iAmServer <macbook@MACBOOKs-MacBook-Air.local>
2019-01-21 08:38:36 -0600

    new commit

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    $output = "output";
+    $input = $output.".mp4";
 
     //make sure the images are local and rename in sequence ex image-1.jpg,image-2.jpg,etc
     //its advice the image should be of the same size for better result
@@ -7,13 +9,20 @@
     //NB if the there's a case that the some pictures are not showing, that's because the picture is not supported
 
     function convertImg2Vid($output){
-        $new = "ffmpeg -y -i image-%d.jpg -vf zoompan=z='(zoom+0.001)':s=1280x720 ".$output.".mp4";
+        $new = "ffmpeg -y -i image-%d.jpg -i audio.mp3 -c:a copy -vf zoompan=z='(zoom+0.0009)':s=1280x720 ".$output.".mp4";
         exec($new);
     }
 
-    $output = "jess";
+    function waterVid($input){
+        $cmd = "ffmpeg -y -i ".$input." -i logo.png -filter_complex 'overlay=W-w-5:H-h-5' new_".$input;
+        exec($cmd);
+        //unlink($input);
+    }
 
     convertImg2Vid($output);
+    waterVid($input);
+    
 
+    //execute using php index.php locally
     //thanks for your trust
 ?>
---
+++ roughwork.php
@@ -0,0 +1,27 @@
+<?php
+// function addwatermark($input){
+    //     $ffmpeg = FFMpeg\FFMpeg::create(array(
+    //         'ffmpeg.binaries' => exec('which ffmpeg'),
+    //         'ffprobe.binaries' => exec('which ffprobe'),
+    //         'timeout'          => 0, // The timeout for the underlying process
+    //         'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
+    //     ));
+
+    //     // $ffmpeg->getFFMpegDriver()->listen(new \Alchemy\BinaryDriver\Listeners\DebugListener());
+    //     // $ffmpeg->getFFMpegDriver()->on('debug', function ($message) {
+    //     //     echo $message."\n";
+    //     // });
+
+    //     $video = $ffmpeg->open("$input");
+
+    //     $video->filters()->watermark('./logo.png', array(
+    //         'position' => 'absolute',
+    //         'x' => 1180,
+    //         'y' => 620,
+    //     ));
+    //     $video->save(new FFMpeg\Format\Video\X264('libmp3lame'), 'new_'.$input);
+    // }
+
+
+       //addwatermark($input);
+    ?>
